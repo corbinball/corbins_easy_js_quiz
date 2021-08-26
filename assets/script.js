b4b20dcfbc5970e59bafe2ef75e4df8224dfc1d0
Corbin Ball <c.ball@me.com>
2021-08-25 23:28:37 -0500

    created vars for questions, correct anwser, and wrong anwsers

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+var questOne = "put question one here";
+var questTwo = "put question two here";
+var questThree = "put question three here";
+var questFour = "put question four here";
+
+var anwOne = "correct here";
+var wrongOne = ["wrongs here"];
+var anwTwo = "correct here";
+var wrongTwo = ["wrongs here"];
+var anwThree = "correct here";
+var wrongThree = ["wrongs here"];
+var anwFour = "correct here";
+var wrongFour = ["wrongs here"];
+
